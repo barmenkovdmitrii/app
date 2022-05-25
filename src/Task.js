5e29384de877f04b41b42bbfc54c123b84414ce8
barmenkovdmitrii <bds@porter.pro>
2022-05-25 15:04:12 +0300

    250522

--- src/Task.js
+++ src/Task.js
@@ -1,13 +1,20 @@
 import './App.css';
 
+let apples = 5;
+
+
 const Task = () => {
     return (
         <div className="App">
+
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
+
                 </p>
+            <p>{apples+2}</p>
         </div>
     );
+
 }
 
 export default Task;
